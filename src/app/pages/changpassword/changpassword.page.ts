cbb7347c27105aa39b7bb143b20ce05393e99e13
Anucha Aeka <keadipod@gmail.com>
2019-04-05 23:41:15 +0700

    update05/04/2019

--- src/app/pages/changpassword/changpassword.page.ts
+++ src/app/pages/changpassword/changpassword.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { NavController, MenuController, LoadingController } from '@ionic/angular';
+import { Router,ActivatedRoute, } from  "@angular/router";
+import { ApiService } from '../../services/api.service';
+import { PasswordValidator } from './password.validator';
 
 @Component({
   selector: 'app-changpassword',
@@ -7,9 +12,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangpasswordPage implements OnInit {
 
-  constructor() { }
+  validations_form: FormGroup;
+  matching_passwords_group: FormGroup;
+
+
+  constructor(
+    public navCtrl: NavController,
+    public menuCtrl: MenuController,
+    public loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder,
+    private router :Router  ,
+    private api :ApiService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+
+    //  We just use a few random countries, however, you can use the countries you need by just adding them to this list.
+    // also you can use a library to get all the countries from the world.
+
+    this.matching_passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.required,
+        ])),
+      confirm_password: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areEqual(formGroup);
+    });
+
+  
+
+    this.validations_form = this.formBuilder.group({
+
+      oldpassword: new FormControl('', Validators.required),
+      matching_passwords: this.matching_passwords_group,
+
+    });
   }
+
+  validation_messages = {
+    
+    'oldpassword': [
+      { type: 'required', message: 'กรอกรหัสผ่านเดิมด้วยค่ะ' },
+    ],
+    'password': [
+      { type: 'required', message: 'กรอกรหัสผ่านใหม่ด้วยค่ะ' },
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'ยืนยันรหัสผ่านใหม่ด้วยค่ะ' }
+    ],
+    'matching_passwords': [
+      { type: 'areEqual', message: 'รหัสผ่านไม่ตรงกันค่ะ' }
+    ],
+
+  }
+
+  async onSubmit(values){
+    //console.log(values);
+
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+      spinner: 'crescent',
+      duration: 2000
+      });
+      await loading.present();
+      await this.api.updateData('account/changpassword',this.route.snapshot.paramMap.get('id'),values)
+      .subscribe(res => {
+     //   console.log(res);
+        loading.onWillDismiss().then(() => {
+          this.api.presentToast(res.message);
+          this.validations_form.reset();
+          this.matching_passwords_group.reset();
+          //this.router.navigate(['dashboard']);
+          
+           });
+      
+        }, (err) => {
+
+          this.api.presentToast('ไม่พบสัญญาณ internet หรือไม่สามารถติดต่อ server ได้');
+
+        });
+}
+
 
 }
--- src/app/pages/editprofile/editprofile.page.ts
+++ src/app/pages/editprofile/editprofile.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { NavController, MenuController, LoadingController } from '@ionic/angular';
+import { Router,ActivatedRoute, } from  "@angular/router";
+import { AuthenService } from '../../services/authen.service';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-editprofile',
@@ -7,9 +12,98 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditprofilePage implements OnInit {
 
-  constructor() { }
+  validations_form: FormGroup;
+  userProfile;
+  img;
+  res;
+
+  constructor(
+    public navCtrl: NavController,
+    public menuCtrl: MenuController,
+    public loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder,
+    private authen :AuthenService,
+    private router :Router  ,
+    private api :ApiService,
+    private route: ActivatedRoute,
+  ) { 
+    this.userProfile =this.api.getStore();
+
+    this.res = this.userProfile.data;
+    this.img=this.userProfile.img;
+  }
 
   ngOnInit() {
+    
+    this.validations_form = this.formBuilder.group({
+
+      username: new FormControl(this.res.u_username, Validators.compose([
+        Validators.pattern('^[a-zA-Z0-9]+$'),
+        Validators.required
+      ])),
+      
+      name: new FormControl(this.res.u_name, Validators.required),
+      lastname: new FormControl(this.res.u_lastname, Validators.required),
+      email: new FormControl(this.res.u_email, Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      phone: new FormControl(this.res.u_tel, Validators.compose([
+        Validators.pattern('^[0-9]+$')
+      ])),
+
+    });
   }
+
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'กรอกชื่อผู้ใช้งานด้วยค่ะ!' },
+      { type: 'pattern', message: 'กรอก a-zA-Z0-9 เท่านั้นค่ะ!' },
+    ],
+    'name': [
+      { type: 'required', message: 'กรอกชื่อด้วยค่ะ!' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'กรอกนามสกุลด้วยค่ะ!' }
+    ],
+    'email': [
+      { type: 'required', message: 'กรอกอีเมลด้วยค่ะ!.' },
+      { type: 'pattern', message: 'กรอกอีเมลให้ถูกต้องด้วยค่ะ' }
+    ],
+    'phone': [
+      { type: 'pattern', message: 'กรอกเฉพาะตัวเลขเท่านั้นค่ะ!' }
+    ],
+    
+
+  }
+
+  async onSubmit(values){
+    //console.log(values);
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+      spinner: 'crescent',
+      duration: 2000
+      });
+      await loading.present();
+      await this.api.updateData('account/updateprofile',this.route.snapshot.paramMap.get('id'),values)
+      .subscribe(res => {
+       
+        loading.onWillDismiss().then(() => {
+          this.api.presentToast(res.message);
+          localStorage.setItem('userData',JSON.stringify(res))
+          this.authen.login(res);
+          this.router.navigate(['dashboard']);
+          
+           });
+      
+        }, (err) => {
+
+          this.api.presentToast('ไม่พบสัญญาณ internet หรือไม่สามารถติดต่อ server ได้');
+
+        });
+
+  
+}
+
 
 }
