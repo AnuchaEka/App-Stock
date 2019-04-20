72923f000e5be8958ece8ddd33cde2a4c18eb367
Anucha Aeka <keadipot@gmail.com>
2019-04-20 18:40:41 +0700

    update 20/04/2019

--- src/app/pages/cutstockhistorylist/cutstockhistorylist.page.ts
+++ src/app/pages/cutstockhistorylist/cutstockhistorylist.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { LoadingController} from '@ionic/angular';
+import { Router,ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cutstockhistorylist',
@@ -7,9 +10,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CutstockhistorylistPage implements OnInit {
 
-  constructor() { }
+  name;
+  id
+  pid;
+  resdata=[];
+  proname;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private api:ApiService,
+    public loadingCtrl: LoadingController,
+  ) { 
+    this.name = this.route.snapshot.paramMap.get('name');
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.pid = this.route.snapshot.paramMap.get('pid');
+  }
+
+  doRefresh(event) {
+    //console.log('Begin async operation');
+    setTimeout(() => {
+     // console.log('Async operation has ended');
+     this.getProd()
+     this.getData();
+     event.target.complete();
+    }, 2000);
+  }
+
 
   ngOnInit() {
+    this.getProd();
+    this.getData();
   }
+
+  async getProd(){
+
+    return await this.api.getDataById('cutstock/getProduct',this.pid)
+      .subscribe(res => {
+
+       // console.log(res);
+        
+        this.proname=res.data['p_name'];
+
+        }, (err) => {
+
+          this.api.showMiddlewareAlert(err)
+        });
+
+  }
+
+
+  async getData() {
+    
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...',
+      spinner: 'crescent',
+      duration: 2000
+    });
+
+    await loading.present();
+    await this.api.getData('cutstock/viewstockallProd/'+this.id+'/'+this.pid)
+      .subscribe(res => {
+        //console.log(res);
+        this.resdata = res;
+        loading.dismiss();
+      }, err => {
+        //console.log(err);
+        loading.dismiss();
+      });
+  }
+
+  
+  
 
 }
